297824155ab38cc5745c75028010a4b8dc52a953
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-03-09 07:15:21 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,24 @@
+`let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+let answer;
+switch (num1 + num2) {
+case 5:
+answer = "5";
+break;
+case 6:
+answer = "6";
+break;
+case 7:
+answer = "7";
+break;
+case 8:
+answer = "8";
+break;
+case 9:
+answer = "9";
+break;
+default:
+answer = "";
+}
+console.log(num1 + num2);
+return answer;`
