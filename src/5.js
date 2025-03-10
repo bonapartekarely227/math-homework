f9021758605c71bcdff29be28a541767cf959c9c
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-03-10 10:13:52 +0000

    Add 5.js

---
+++ src/5.js
@@ -0,0 +1,8 @@
+<script>
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  const num = getRandomInt(100);
+  console.log("Your random number is: ", num);
+</script>
