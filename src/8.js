abe0d4f852bd81728472aefabb212ae13aa75962
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-03-14 19:14:09 +0000

    Add 8.js

---
+++ src/8.js
@@ -0,0 +1,7 @@
+  let x = Math.random() * 10;
+  let y = Math.random() * 10;
+  if (x > y) {
+    console.log("The value of x is greater than the value of y.");
+  } else {
+    console.log("The value of y is greater than or equal to the value of x.");
+  }
