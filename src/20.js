17eb801e4a054554a95dfb12be8d5167ce2a29e7
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-03-31 16:26:06 +0000

    Add 20.js

---
+++ src/20.js
@@ -0,0 +1,7 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+console.log(getRandomInt(10)); // Example output: 4
+
+// You can use this function to generate random numbers for your project
