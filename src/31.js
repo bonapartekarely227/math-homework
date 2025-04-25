1eb72613b79d54319cd15c1c179bb44882132b8e
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-04-25 15:36:46 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,6 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage: Get a random number between 1 and 10
+console.log(getRandomInt(1, 10));
