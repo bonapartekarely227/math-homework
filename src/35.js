91e8e4ea5582b0be2d741d6d23312f0295371f92
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-05-19 08:04:24 +0000

    Add 35.js

---
+++ src/35.js
@@ -0,0 +1,4 @@
+// Example of generating random code
+let randomNumber = Math.random();
+let uppercaseLetter = String.fromCharCode(65 + randomNumber % 26);
+console.log(`Generated random letter: ${uppercaseLetter}`);
