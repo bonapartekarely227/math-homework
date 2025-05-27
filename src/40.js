5094d863638dee4c3171a665782068cffd5dc012
bonapartekarely227 <bonapartekarely227@gmail.com>
2025-05-27 15:02:05 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,6 @@
+function calculateArea(radius) {
+    const area = Math.PI * radius * radius;
+    console.log(`The area of the circle with radius ${radius} is ${area.toFixed(2)} square units.`);
+}
+
+calculateArea(5);
